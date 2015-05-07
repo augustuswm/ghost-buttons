43a2f0038b16515a97524da13a5a5e26e0613457
augustuswm <gusmayo@gmail.com>
2015-05-06 21:01:22 -0500

    Initial commit

---
+++ assets/js/Article.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Article = React.createClass({
+  render: function() {
+    return (
+      <div>
+        Hello World
+      </div>
+    );
+  }
+});
+
+module.exports = Article;
