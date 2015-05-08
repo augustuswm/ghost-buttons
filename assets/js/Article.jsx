a075e22f3f1e8cf12b266e9262328f33bc692b01
Augustus Mayo <gusmayo@gmail.com>
2015-05-07 23:43:02 -0500

    Button actions

--- assets/js/Article.jsx
+++ assets/js/Article.jsx
@@ -3,22 +3,55 @@ var React = require('react'),
     ArticleMeta = require('./ArticleMeta.jsx'),
     ArticleBody = require('./ArticleBody.jsx');
 
+var ArticleStore = {
+  "post-one": {
+    title: "This is the first post",
+    meta: {
+      date: "01-01-2001",
+      tags: [
+        {
+          slug: "coop-gaming",
+          description: "All about cooperative gaming",
+          name: "Coop"
+        }
+      ]
+    },
+    body: "This is some body copy all about the first post on the blog."
+  },
+  "post-two": {
+    title: "Another post",
+    meta: {
+      date: "01-01-2099",
+      tags: [
+        {
+          slug: "not-coop-gaming",
+          description: "Nothing about cooperative gaming",
+          name: "Not Coop"
+        }
+      ]
+    },
+    body: "This is another post, likely the second one made."
+  }
+};
+
 var Article = React.createClass({
-  getInitialState: function() {
+  getEmptyState: function() {
     return {
       title: "",
       meta: {
-        date: "01-01-2001",
-        tags: [
-          {
-            slug: "coop-gaming",
-            description: "All about cooperative gaming",
-            name: "Coop"
-          }
-        ]
+        date: "",
+        tags: []
       },
-      body: "This is some body copy."
+      body: ""
     };
+  },
+  getInitialState: function() {
+    return ArticleStore[this.props.slug] || this.getEmptyState();
+  },
+  componentWillReceiveProps: function(nextProps) {
+    if (this.props.slug !== nextProps.slug) {
+      this.setState(ArticleStore[nextProps.slug] || this.getEmptyState());
+    }
   },
   render: function() {
     var classes = "article-container" + ((this.state.title) ? " article-active" : "");
